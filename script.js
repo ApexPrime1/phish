78da375f908ef215b6457967af4a48c1bc241e1e
ApexPrime1 <135202570+ApexPrime1@users.noreply.github.com>
2023-11-30 09:58:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("instagram-icon").addEventListener("click", function() {
+    window.location.href = "instagram.html";
+});
+
+document.getElementById("email-icon").addEventListener("click", function() {
+    window.location.href = "email.html";
+});
